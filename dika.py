bbfb75ec5b3fbf6a561da9b7758ae3a5e48110f0
dikk <143859991+andika-k@users.noreply.github.com>
2023-09-02 10:56:11 +0700

    Add files via upload

---
+++ dika.py
@@ -0,0 +1,52 @@
+#Variabel dan tipe data
+
+x = 5 #integer
+y = 10
+z = "saya" #string
+a = "99"
+#Float
+b = 3.14
+#Boolean True or False 
+
+#Aritmatika 
+
+c = x + y 
+print("hasil penjumlahan",c)
+
+u = x - y
+print("hasil pengurangan",u)
+
+v = x * y
+print("hasil perkalian",v)
+
+z = x / y 
+print("hasil pembagian",z)
+
+k = x ** y 
+print("hasil perpangkatan",k)
+
+m = x % y 
+print("hasil modulus", m)
+
+nama_saya = x * y * y
+print("hasil perkalian",nama_saya)
+
+ya_ = y * y / y 
+print("hasil dari",ya_)
+
+ux_ = y + y * y - y ** y
+print("hasil", ux_)
+
+uz_ = y * y ** y % y 
+print("hasilnya adalah", uz_)
+
+
+
+nama = int(input("masukkan angka"))
+nama2 = float(input("masukkan angka")) 
+hitung = nama + nama2              
+print(nama) 
+print(nama2) 
+print(hitung)
+
+l = int(input("hasil luas persegi panjang, hitung"))
